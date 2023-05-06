da7a8069553bf9fbf2e9881103902e43b6d5a178
Brian <b.lee@utexas.edu>
2023-05-06 17:43:28 +0900

    add data augmentations to EDA

--- experiments/EDA.py
+++ experiments/EDA.py
@@ -7,6 +7,8 @@
 import random
 import plotly.express as px
 from torch_geometric.datasets import ShapeNet
+from torch_geometric.loader import DataLoader
+import torch_geometric.transforms as T
 
 # %% Load the ShapeNet dataset and take subset
 ''' 
@@ -27,6 +29,7 @@ def plot_3d_shape(shape):
     y = shape.pos[:, 1]
     z = shape.pos[:, 2]
     fig = px.scatter_3d(x=x, y=y, z=z, opacity=0.3)
+    fig.update_traces(marker_size=3)
     fig.show()
 
 sample_idx = random.choice(range(len(dataset)))
@@ -37,3 +40,17 @@ cat_dict = {key: 0 for key in dataset.categories}
 for datapoint in dataset:
     cat_dict[dataset.categories[datapoint.category.int()]] += 1
 print(cat_dict)
+
+# %% Apply data augmentation on a sample and visualize
+dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+augmentation = T.Compose([T.RandomJitter(0.005), T.RandomFlip(1), T.RandomShear(0.3)])
+
+sample = next(iter(dataloader))
+plot_3d_shape(sample[0]) # Original
+
+augmented_sample = augmentation(sample)
+plot_3d_shape(augmented_sample[0]) # Augmented
+
+# %%
+plot_3d_shape(dataset[170]) # Motorbike
+plot_3d_shape(dataset[181]) # Desk
