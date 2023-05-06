ca48452cc938db51a5a662953e50d2171a09e690
Brian <b.lee@utexas.edu>
2023-05-06 17:59:37 +0900

    added example inference using dataset for model.pyg

--- src/model.py
+++ src/model.py
@@ -3,7 +3,6 @@ import torch
 import torch.nn as nn
 from torch.nn import Linear
 from torch_geometric.nn import MLP, DynamicEdgeConv, global_max_pool
-import torch_geometric.transforms as T
 
 # %%
 class SimCLR(nn.Module):
@@ -28,22 +27,22 @@ class SimCLR(nn.Module):
             augm_2 = self.augmentation(data)
 
             # Extract properties
-            pos_1, batch_1 = augm_1.pos, augm_1.batch
-            pos_2, batch_2 = augm_2.pos, augm_2.batch
+            pos1, batch1 = augm_1.pos, augm_1.batch
+            pos2, batch2 = augm_2.pos, augm_2.batch
 
             # Get representations for first augmented view
-            x1 = self.conv1(pos_1, batch_1)
-            x2 = self.conv2(x1, batch_1)
+            x1 = self.conv1(pos1, batch1)
+            x2 = self.conv2(x1, batch1)
             h_points_1 = self.lin1(torch.cat([x1, x2], dim=1))
 
             # Get representations for second augmented view
-            x1 = self.conv1(pos_2, batch_2)
-            x2 = self.conv2(x1, batch_2)
+            x1 = self.conv1(pos2, batch2)
+            x2 = self.conv2(x1, batch2)
             h_points_2 = self.lin1(torch.cat([x1, x2], dim=1))
             
             # Global representation
-            h_1 = global_max_pool(h_points_1, batch_1)
-            h_2 = global_max_pool(h_points_2, batch_2)
+            h1 = global_max_pool(h_points_1, batch1)
+            h2 = global_max_pool(h_points_2, batch2)
 
         else:
             x1 = self.conv1(data.pos, data.batch)
@@ -52,6 +51,23 @@ class SimCLR(nn.Module):
             return global_max_pool(h_points, data.batch)
 
         # Transformation for loss function
-        compact_h_1 = self.mlp(h_1)
-        compact_h_2 = self.mlp(h_2)
-        return h_1, h_2, compact_h_1, compact_h_2
+        compact_h1 = self.mlp(h1)
+        compact_h2 = self.mlp(h2)
+        return h1, h2, compact_h1, compact_h2
+
+# %%
+if __name__=='__main__':
+    import torch_geometric.transforms as T
+    from torch_geometric.datasets import ShapeNet
+
+    dataset = ShapeNet(root='../data/ShapeNet/', categories=['Table', 'Lamp', 'Guitar', 'Motorbike']).shuffle()[:5000]
+
+    augmentation = T.Compose([T.RandomJitter(0.005), T.RandomFlip(1), T.RandomShear(0.3)])
+    model = SimCLR(augmentation=augmentation)
+
+    h1, h2, compact_h1, compact_h2 = model(dataset[0])
+    
+    print('h1: ', h1)
+    print('h2: ', h2)
+    print('compact_h1: ', compact_h1)
+    print('compact_h2: ', compact_h2)
