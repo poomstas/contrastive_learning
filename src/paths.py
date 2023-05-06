9fc47e15dab6dbc76b441fcde26eeeeed94b8495
Brian <b.lee@utexas.edu>
2023-05-06 19:18:48 +0900

    set up paths.py file

--- src/paths.py
+++ src/paths.py
@@ -5,7 +5,7 @@ import os
 ''' Contains all the paths used in the project. '''
 
 # Change PROJECT_ROOT accordingly
-PROJECT_ROOT = '/home/brian/github/lightning_template/'
+PROJECT_ROOT = '/home/brian/github/contrastive_learning_point_cloud'
 
-DATA = os.path.join(PROJECT_ROOT, 'data')
+DATA = os.path.join(PROJECT_ROOT, 'data/ShapeNet')
 SOURCE = os.path.join(PROJECT_ROOT, 'src')
