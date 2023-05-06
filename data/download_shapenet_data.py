5cb53428a9b7019df4a1220070306f59a4992bcb
Brian <b.lee@utexas.edu>
2023-05-06 12:17:33 +0900

    added instructions for setting up environment before downloading datag

--- data/download_shapenet_data.py
+++ data/download_shapenet_data.py
@@ -1,3 +1,4 @@
+''' Make sure to set up the environment before running this script. See ../txt/requirements.txt '''
 from torch_geometric.datasets import ShapeNet
 
 dataset = ShapeNet(root='./ShapeNet', categories=['Table', 'Lamp', 'Guitar', 'Motorbike']).shuffle()[:5000]
