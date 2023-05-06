b224852ee07b60a73c8ba13ccb5c25bae5ec4e16
Brian <b.lee@utexas.edu>
2023-05-06 12:19:22 +0900

    wrote EDA code to download and visualize ShapeNet subset data

--- experiments/EDA.py
+++ experiments/EDA.py
@@ -1,14 +1,39 @@
 # %%
-import os
-import numpy as np
-import pandas as pd
-
-import sys
-sys.path.append('..')
-
-from src.paths import DATA
+'''
+    Download the ShapeNet dataset, take a subset, and take a look at it to see how the data is structured.
+'''
 
 # %%
-os.listdir(DATA)
+import random
+import plotly.express as px
+from torch_geometric.datasets import ShapeNet
 
-# %%
+# %% Load the ShapeNet dataset and take subset
+''' 
+    pos: Normalized positions as 3D coordinates 
+    x : Normal vectors (unused in this study)
+    y : ~~ (unused in this study)
+    category: ~~ (used later to validate)
+'''
+
+dataset = ShapeNet(root='../data/ShapeNet/', categories=['Table', 'Lamp', 'Guitar', 'Motorbike']).shuffle()[:5000]
+print(dataset)
+print('Sample: ', dataset[0])
+
+# %% Plot 3D point cloud
+def plot_3d_shape(shape):
+    print('Number of data points: ', shape.x.shape[0])
+    x = shape.pos[:, 0]
+    y = shape.pos[:, 1]
+    z = shape.pos[:, 2]
+    fig = px.scatter_3d(x=x, y=y, z=z, opacity=0.3)
+    fig.show()
+
+sample_idx = random.choice(range(len(dataset)))
+plot_3d_shape(dataset[sample_idx])
+
+# %% Check the distribution of each class
+cat_dict = {key: 0 for key in dataset.categories}
+for datapoint in dataset:
+    cat_dict[dataset.categories[datapoint.category.int()]] += 1
+print(cat_dict)
